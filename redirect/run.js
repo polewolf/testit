1ed49c83179f00ba1e020fdbc0e2adaccbaba4ea
polewolf <22129@hetstreek.nl>
2025-10-03 13:50:49 +0200

    Create run.js

---
+++ redirect/run.js
@@ -0,0 +1,22 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const PORT = 3000;
+
+http.createServer((req, res) => {
+    // Pad naar je index.html
+    const filePath = path.join(__dirname, "./index.html");
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(500, { "Content-Type": "text/plain" });
+            res.end("Error loading index.html");
+        } else {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(data);
+        }
+    });
+}).listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
